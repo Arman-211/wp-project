a8d6f72b44d72e322d1a591c04ef87bc1fd80f80
Arman <armanabajyan95@gmail.com>
2025-03-03 13:48:53 +0400

    add footer

--- wp-content/themes/boomerang/footer.php
+++ wp-content/themes/boomerang/footer.php
@@ -1,5 +1,58 @@
-<footer>
-    <p>© 2025 Boomerang Casino</p>
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer__top">
+            <div class="footer__languages">
+                <a href="/de/" class="lang__item"><img src="<?= get_theme_file_uri() . '/assets/footer/de.svg' ?>" alt="Deutsch"></a>
+                <a href="/en/" class="lang__item active"><img src="<?= get_theme_file_uri() . '/assets/footer/en.svg' ?>" alt="English"></a>
+                <a href="#" class="footer__support">Support</a>
+            </div>
+        </div>
+
+        <div class="footer__payments">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_visa.svg' ?>" alt="Visa">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_mastercard.svg' ?>" alt="Mastercard">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/payment_system_footer_Blik.svg' ?>" alt="Blik">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_postepay.svg' ?>" alt="Postepay">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_skrill.svg' ?>" alt="Skrill">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_neteller.svg' ?>" alt="Neteller">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_interaconline.svg' ?>" alt="Interaconline">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_mifinity.svg' ?>" alt="Mifinity">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_banktransfer.svg' ?>" alt="Banktransfer">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_paysafecard.svg' ?>" alt="Paysafecard">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_bitcoin.svg' ?>" alt="Bitcoin">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_litecoin.svg' ?>" alt="Litecoin">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_ethereum.svg' ?>" alt="Ethereum">
+            <img class="footer__payments_img" src="<?= get_theme_file_uri() . '/assets/footer/paymsystem_footer_ripple.svg' ?>" alt="Ripple">
+        </div>
+
+        <!-- Навигация -->
+        <nav class="footer__nav">
+            <a href="/contact-us">Contact Us</a>
+            <a href="/faq">FAQ</a>
+            <a href="/about-us">About BoomerangCasino</a>
+            <a href="/terms">Terms and Conditions</a>
+            <a href="/privacy-policy">Privacy Notice</a>
+            <a href="/cookies-policy">Cookie Notice</a>
+            <a href="http://boomerang-partners.com/" target="_blank">Partners</a>
+            <a href="/sitemap">Sitemap</a>
+        </nav>
+
+        <!-- Лицензии и ответственность -->
+        <div class="footer__licenses">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/18.svg' ?>" alt="18">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/boomeranglogopartner.svg' ?>" alt="boomeranglogopartner">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/license.svg' ?>" alt="license">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/boom2.svg' ?>" alt="boom2">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/guru.png' ?>" alt="guru">
+            <img class="footer__licenses_img" src="<?= get_theme_file_uri() . '/assets/footer/cashofslotsicon.svg' ?>" alt="cashofslotsicon">
+        </div>
+
+        <!-- Подвал -->
+        <div class="footer__bottom">
+            <p>Gambling can be addictive. Play responsibly. BoomerangCasino.com only accepts customers over 18 years of age</p>
+            <p>2025 © BoomerangCasino.com All rights reserved</p>
+        </div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
