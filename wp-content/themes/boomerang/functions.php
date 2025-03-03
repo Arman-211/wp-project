33583fe96975284c8c37b3161002b9732109d1a8
Arman <armanabajyan95@gmail.com>
2025-03-03 17:55:58 +0400

    styles correct in footer and slots

--- wp-content/themes/boomerang/functions.php
+++ wp-content/themes/boomerang/functions.php
@@ -190,7 +190,7 @@ function slots_shortcode(): bool|string
                     </div>
                     <div class="slot-info">
                         <span class="slot-provider"><?php echo htmlspecialchars($slot['provider']); ?></span>
-                        <div class="slot-favorite"> <img class="img_slot_hover"
+                        <div class="slot-favorite"> <img class="img_slot_heart"
                                                          src="<?php echo get_theme_file_uri() . '/' . "assets/heart.svg" ?>"
                                                          alt="<?php echo htmlspecialchars("assets/heart.svg"); ?>"></div>
                     </div>
